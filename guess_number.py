f3556bd9932bedd9bcd66d2ce1d2fdf2f83ce1d7
Evan Wang <wangxevan@gmail.com>
2020-06-10 23:01:50 +0800

    final Version

--- guess_number.py
+++ guess_number.py
@@ -4,28 +4,28 @@
 # 猜錯的話 要告訴他 比答案大/小
 # 印出猜了幾次
 # 讓使用者決定隨機數範圍
-n1 = input ("隨機數起始值")
-n2 = input ("隨機數結束值")
-n1 = int(n1)
-n2 = int(n2)
+start = input ("隨機數起始值：") # 變數名稱不要亂取，盡量取意思相近詞
+end = input ("隨機數結束值：")  
+start = int(start)  # 型態轉換成 整數
+end = int(end)
 import random
-r =  random.randint(n1,n2)
-print (r)
-time = 0
+r =  random.randint(start,end)
+count = 0  # 計算猜的次數 ，需要在while迴圈外，否則會一直歸零
 while True :
 	num = input ("請猜數字  ： ")
 	num = int(num)
-	time = time + 1
+	count += 1   # 意思就是 count = count + 1  
 	if num == r :
 		print ("終於猜對了")
-		print ("你猜了",time,"次")
+		print ("你猜了", count,"次")
 		break
 	elif num > r :
-		
 		print ("比答案大")
-	else :
 
+	elif num < r :
 		print ("比答案小")
+
+	print ("你猜了", count,"次")  # 寫elif外圈 ，避免重覆寫二次
  	    
  			
 
